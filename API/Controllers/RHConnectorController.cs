5460e6c2b9e5283330aa81b52e0995dc0ac9d91c
Aluxann <jlatorrel@ucenfotec.ac.cr>
2026-02-25 13:20:24 -0600

    Add project files.

---
+++ API/Controllers/RHConnectorController.cs
@@ -0,0 +1,32 @@
+﻿using AppLogic;
+using DTO;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [EnableCors("DemoPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RHConnectorController : ControllerBase
+    {
+        private readonly IRHConnector _rhConnector;
+
+        public RHConnectorController(IRHConnector rhConnector)
+        {
+            _rhConnector = rhConnector;
+        }
+
+        [HttpGet("GetAllEmployees")]
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await _rhConnector.RetrieveAllEmployees();
+        }
+
+        [HttpGet("GetAllSpecialties")]
+        public async Task<List<string>> GetAllSpecialties()
+        {
+            return await _rhConnector.RetrieveAllSpecialties();
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors();
+
+app.Run();
